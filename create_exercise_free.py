0f6d0a09cfd4f74cd3f76b86a94a25f7ecc3bcd2
marcofattorelli <marco.fattorelli@studenti.univr.it>
2020-07-26 12:40:09 +0200

    allineati: user_bar, download_b, create_exe

--- create_exercise_free.py
+++ create_exercise_free.py
@@ -113,7 +113,7 @@ def insert_description1(note, exer_descr1, exam_date):
     img_check = False
     if look_for_img(exer_descr1) == 1:
         exer_descr1 = exer_descr1.replace("img_" + exam_date, "img")
-        download_button_lib = download_button_lib.replace('@img_name@',images_to_add[-1])
+        download_button_lib = download_button_lib.replace('@img_name@',images_to_add[-1]).replace('@path_ex_folder@',path_ex_folder)
         img_check = True
     #print(exer_descr1)
     note['cells'] += [nb.v4.new_markdown_cell(exer_descr1)]
@@ -137,7 +137,7 @@ def insert_user_bar_lib(note):
     Parameters:
     note (Jupyter nb.v4): the notebook"""
     user_bar_lib = open(PATH_UTILS + 'user_bar.py', 'r', encoding='utf-8').read()
-    note['cells'] += [nb.v4.new_code_cell(user_bar_lib)]
+    note['cells'] += [nb.v4.new_code_cell(user_bar_lib.replace('@path_ex_folder@',path_ex_folder))]
     note.cells[-1].metadata = {"init_cell": True, "editable": False, "deletable": False, "tags": ['run_start']}
     return
 
